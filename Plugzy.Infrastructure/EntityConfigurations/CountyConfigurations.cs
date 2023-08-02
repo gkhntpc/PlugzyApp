951403c2dc230458d89656b3599c76d035c8600e
Şafak <safak.akdemir@nextlua.com>
2023-08-02 14:10:00 +0300

    EntityConfigurations Added.

---
+++ Plugzy.Infrastructure/EntityConfigurations/CountyConfigurations.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Plugzy.Domain.Entities;
+
+namespace Plugzy.Infrastructure.EntityConfigurations
+{
+    public class CountyConfigurations : IEntityTypeConfiguration<County>
+    {
+        public void Configure(EntityTypeBuilder<County> builder)
+        {
+            builder.Property(b => b.Name).HasMaxLength(100);
+        }
+    }
+}
---
+++ Plugzy.Infrastructure/EntityConfigurations/OtpConfigurations.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Plugzy.Domain.Entities;
+namespace Plugzy.Infrastructure.EntityConfigurations
+{
+    public class OtpConfigurations : IEntityTypeConfiguration<Otp>
+    {
+        public void Configure(EntityTypeBuilder<Otp> builder)
+        {
+            builder.Property(b => b.Phone).HasMaxLength(100);
+        }
+    }
+}
